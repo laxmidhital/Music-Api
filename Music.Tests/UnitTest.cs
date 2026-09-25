e42e6cd5b0e105f903980db0d06b1b20bd37fa8d
Laxmi <laxmidh2001@gmail.com>
2026-09-25 11:34:34 +0200

    adding-test-files fikser feil

---
+++ Music.Tests/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Music.Controllers;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+
+namespace Music.Tests;
+public class UnitTest
+{
+    [Fact]
+    public void Get_ReturnsAlbums()
+    {
+        // Arrange
+        var secretClient = new SecretClient(
+            new Uri("https://test.vault.azure.net/"),
+            new DefaultAzureCredential());
+
+        var controller = new MusicController(secretClient);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(Album.Albums.Count, result.Count);
+    }
+}
